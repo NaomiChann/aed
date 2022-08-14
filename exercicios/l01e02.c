72b606db231819b53f6ecaf018b5fd177bdbe4d2
NaomiChann <naomi.cmyk@gmail.com>
2022-08-14 00:38:08 -0300

    50% done

--- exercicios/l01e02.c
+++ exercicios/l01e02.c
@@ -12,8 +12,81 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME_SIZE 22
+
+char* NameAdd( char *list );
+void NameRemove();
 
 int main() {
-    
+    int input, userCheck = 1;
+    char *list;
+    list = ( char * ) malloc( sizeof(char) );
+    list[0] = '\0';
+
+    while ( userCheck == 1 ) {
+        printf( "1) Adicionar nome \n2) Remover nome \n3) Listar \n4) Sair \n" );
+        scanf( "%d", &input );
+        getc(stdin);
+
+        switch ( input ) {
+            case 1:
+                NameAdd( list );
+                break;
+
+            case 2:
+                NameRemove();
+                break;
+
+            case 3:
+                printf( "%s", list );
+                break;
+
+            case 4:
+                userCheck = 0;
+                free( list );
+                break;
+            
+            default:
+                break;
+        }
+    }
+
     return 0;
+}
+
+/*
+====================
+NameAdd
+ Adds name to the list string
+====================
+*/
+char* NameAdd( char *list ) {
+    char name[MAX_NAME_SIZE], newLine[2] = "\n";
+
+    printf( "Digite o nome a ser adicionado: " );
+    fgets( name, MAX_NAME_SIZE - 1, stdin );
+    fflush( stdin );
+
+    if ( strchr( name, '\n' ) == NULL ) {
+        strcat( name, newLine );
+    }
+
+    list = ( char * ) realloc( list, ( strlen( list ) + strlen( name ) + 1 ) * sizeof( char ));
+
+    strcat( list, name );
+
+    return list;
+}
+
+/*
+====================
+NameRemove
+ Removes name from the list string
+====================
+*/
+void NameRemove() {
+    printf( "Digite o nome a ser removido: " );
+
 }
