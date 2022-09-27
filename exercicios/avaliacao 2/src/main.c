8f32a94603548e8c90b8c6ecd816376a01b27756
NaomiChann <naomi.cmyk@gmail.com>
2022-09-26 22:38:35 -0300

    changed stack from dynamic to a stactic array

--- exercicios/avaliacao 2/src/main.c
+++ exercicios/avaliacao 2/src/main.c
@@ -1,8 +1,8 @@
 /*
 fazer um programa que testa se uma sequencia de caracteres fornecida pelo usuario é
 um palindromo, ou seja, é uma palavra cuja primeira metade é simetrica à segunda metade
-exemplo - aabccbaa
-o usuario digita uma letra e a tecla enter a´te que ele digite x e enter para terminar a sequencia
+exemplo - aabccbaa = sim
+o usuario digita uma letra e a tecla enter ate que ele digite x e enter para terminar a sequencia
 implemente a função pop e push de pilha e resolva o problema com elas
 no video 4 casos de s e n
 */
@@ -21,19 +21,18 @@ StackInfo Pop( char *stack, StackInfo info );
 
 int main() {
 	StackInfo info, infoCheck;
+	char input, stack[100], stackCheck[100];
 	info.size = 0;
 	infoCheck.size = 0;
-
-	char input, *stack, *stackCheck;
-	stack = ( char * ) malloc( 1 );
-	stackCheck = ( char * ) malloc( 1 );
+	info.bot = &stack[0];
+	infoCheck.bot = &stackCheck[0];
 
 	printf( "\nDigite os caracteres ( ou digite x para sair )\n" );
 	while ( input != 'x' ) {
 		scanf( "%c", &input );
 		fflush( stdin );
 		if ( input != 'x' ) {
-			info = Push( input, stack, info );
+			info = Push( input, &stack[0], info );
 		}
 	}
 
@@ -47,20 +46,20 @@ int main() {
 
 	if ( ( info.size % 2 ) != 0 ) {
         for ( int i = ( info.size - 1 ) / 2; i > 0; i-- ) {
-            infoCheck = Push( *info.top, stackCheck, infoCheck );
-            info = Pop( stack, info );
+            infoCheck = Push( *info.top, &stackCheck[0], infoCheck );
+            info = Pop( &stack[0], info );
         }
-        info = Pop( stack, info );
+        info = Pop( &stack[0], info );
     } else {
         for ( int i = info.size / 2; i > 0; i-- ) {
-            infoCheck = Push( *info.top, stackCheck, infoCheck );
-            info = Pop( stack, info );
+            infoCheck = Push( *info.top, &stackCheck[0], infoCheck );
+            info = Pop( &stack[0], info );
         }
     }
 
 	while ( *info.top == *infoCheck.top && !( info.size == 0 || infoCheck.size == 0 ) ) {
-        info = Pop( stack, info );
-        infoCheck = Pop( stackCheck, infoCheck );
+        info = Pop( &stack[0], info );
+        infoCheck = Pop( &stackCheck[0], infoCheck );
     }
     
     if ( info.size == 0 && infoCheck.size == 0 ) {
@@ -69,18 +68,14 @@ int main() {
         printf( "nao \n" );
     }
 
-	free( stack );
-	free( stackCheck );
 	return ( 0 );
 }
 
 StackInfo Push( char input, char *stack, StackInfo info ) {
 	info.size += 1;
-	stack = realloc( stack, info.size * sizeof( char ) );
-	info.bot = stack;
-	info.top = info.bot + ( info.size - 1 );
-
+	info.top = &stack[info.size - 1];
 	*info.top = input;
+
 	return ( info );
 }
 
@@ -91,10 +86,7 @@ StackInfo Pop( char *stack, StackInfo info ) {
     }
 
 	info.size -= 1;
-	if ( info.size != 0 ) {
-		stack = realloc( stack, info.size * sizeof( char ) );
-	}
-	info.top = info.bot + ( info.size - 1 );
+	info.top = &stack[info.size - 1];
 
 	return ( info );
 }
