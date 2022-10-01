5cc469449e41679255433ad292c691608f26cb4e
NaomiChann <naomi.cmyk@gmail.com>
2022-09-30 23:53:19 -0300

    still not there, pushing just to leave a record

--- agenda/src/main.c
+++ agenda/src/main.c
@@ -28,19 +28,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE_BUFFER ( ( 5 * sizeof( int ) ) + ( 11 * sizeof( char ) ) )
-#define SIZE_NODE ( ( 12 * sizeof( char ) ) + ( 2 * sizeof( int ) ) )
+#define SIZE_BUFFER ( ( 5 * sizeof( int ) ) + ( 10 * sizeof( char ) ) + ( 3 * sizeof( char * ) ) )
+#define SIZE_NODE ( ( 10 * sizeof( char ) ) + ( 2 * sizeof( int ) ) + ( 2 * sizeof( char * ) ) )
 
 	// buffer
-	// |count|input|test|test2|i|name[10]|*head|
-	// 0     1     2    3     4 5					* int        
-	// 							      + 10 	  11	* char 
+	// |count|input|test|test2|i|name[10]|*head|*dum1|*dum2|
+	// 0     1     2    3     4 5							 * int        
+	// 							      + 10					 * char
+	//										 + 1	 2     3 * char*
 
 	// node
 	// |name[10]|age|phone|*prev|*next|
-	// 0        10						* char
-	//			  + 1     2 			* int
-	//						  + 1     2	* char
+	// 0        10						  * char
+	//			  + 1     2 			  * int
+	//						   + 1      2 * char*
 
 void Push( int *userCount );
 void Pop( int *userCount );
@@ -50,30 +51,37 @@ void ContactList( int *userCount );
 
 
 int main() {
-	// |count|input|test|test2|i|name[10]|*head|
-	// 0     1     2    3     4 5					* int        
-	// 							      + 10 	  11	* char
+	// buffer
+	// |count|input|test|test2|i|name[10]|*head|*dum1|*dum2|*dum3|
+	// 0     1     2    3     4 5							 	   * int        
+	// 							      + 10					 	   * char
+	//										 + 1	 2     3     4 * char*
 
+	// node
 	// |name[10]|age|phone|*prev|*next|
-	// 0        10						* char
-	//			  + 1     2 			* int
-	//						  + 1     2	* char
+	// 0        10						  * char
+	//			  + 1     2 			  * int
+	//						   + 1    2 * char*
 
 	void *pBuffer;
-	char *dummy;
-
 	pBuffer = malloc( SIZE_BUFFER );
 
 	int *userCount = pBuffer, *input = userCount + 1;
 	*userCount = 0;
 	*input = 0;
 
-	// set head's pointer as null
-	dummy = pBuffer;
-	( int * ) dummy += 5;
-	dummy += 10; // char
-	*dummy = NULL;
+	char **head = ( char ** ) ( userCount + 5 ), **dummy, **dummyHelper, **dummyHelper2;
+	head = ( char ** ) ( ( char * ) head + 10 );
+	dummy = head + 1;
+	dummyHelper = dummy + 1;
+	dummyHelper2 = dummyHelper + 1;
 
+	// set pointers to null
+	*head = NULL;
+	*dummy = NULL;	
+	*dummyHelper = NULL;
+	*dummyHelper2 = NULL;
+	
 	while ( 1 ) {
         printf( "\nEscolha uma acao: \n1) Adicionar contato \n2) Remover contato \n3) Procurar \n4) Listar \n5) Sair \n" );
         scanf( "%d", input );
@@ -116,14 +124,17 @@ Push
 ====================
 */
 void Push( int *userCount ) {
-	// |count|input|test|test2|i|name[10]|*head|
-	// 0     1     2    3     4 5					* int        
-	// 							      + 10 	  11	* char 
+	// buffer
+	// |count|input|test|test2|i|name[10]|*head|*dum1|*dum2|
+	// 0     1     2    3     4 5							 * int        
+	// 							      + 10					 * char
+	//										 + 1	 2     3 * char*
 
+	// node
 	// |name[10]|age|phone|*prev|*next|
-	// 0        10						* char
-	//			  + 1     2 			* int
-	//						  + 1     2 * char
+	// 0        10						  * char
+	//			  + 1     2 			  * int
+	//						   + 1    2 * char*
 	
 	if ( *userCount == 10 ) {	// full case
 		printf( "Lista cheia. \n" );
@@ -131,81 +142,85 @@ void Push( int *userCount ) {
 	}
 
 	int *i = userCount + 4;
-	char *name = ( char * ) i + sizeof( int ), *head, *node, *dummy, *dummyHelper;
-	head = userCount;
-	( int * ) head += 5;
-	head += 10; // char
+	char *name = ( char * ) ( i + 1 ), **head = ( char ** ) ( userCount + 5 ), *node, **dummy, **dummyHelper;
+	head = ( char ** ) ( ( char * ) head + 10 );
+	dummy = head + 1;
+	dummyHelper = dummy + 1;
 
+	// setup new node
 	node = ( char * ) malloc( SIZE_NODE );
-	dummy = node;
+	*dummy = node;
 
 	printf( "Digite o nome a ser adicionado: " );
     scanf( "%9s", name );	// swap w fgets for spaces
     fflush( stdin );
 
-	name += 9;
+	name += 9; // char
 	*name = '\0'; // just to be safe
-	name -= 9;
+	name -= 9; // char
 
-	*dummy = *name;
-	dummy += 10; // char
+	**dummy = *name;
+	*dummy += 10; // char
 
 	printf( "Digite a idade: " );
-	scanf( "%d", ( int * ) dummy );
+	scanf( "%d", ( int * ) *dummy );
 	fflush( stdin );
 
-	( int * ) dummy += 1;
+	*dummy = ( char * ) ( ( int * ) *dummy + 1 );
 
 	printf( "Digite o telefone: " );
-	scanf( "%d", ( int * ) dummy );
+	scanf( "%d", ( int * ) *dummy );
 	fflush( stdin );
 
+	// transfer new node to list
 	if ( *userCount > 0 ) {	// middle case
-		dummy = *head;
+		dummy = ( char ** ) *head;
 
 		// alphabetical sorting
 		for ( *i = 0; *i < *userCount; *i += 1 ) {
-			if ( strcmp( *name, *dummy ) > 0 ) {	// if after
-				dummy += 11; // char
-				( int * ) dummy += 2;
-				if ( *dummy != NULL ) {
-					dummyHelper = *dummy;
-					dummy = dummyHelper;
+			if ( strcmp( name, *dummy ) > 0 ) {	// if it should be after
+				dummy = ( char ** ) ( ( char * ) dummy + 10 );
+				dummy = ( char ** ) ( ( int * ) dummy + 2 );
+				dummy += 1;	// *next
+				if ( *dummy != NULL ) {	// if there is a next node
+					*dummyHelper = *dummy;
+					dummy = ( char ** ) *dummyHelper;	// next is the current node
 				} else {
-					*dummy = node;
+					*dummy = node;	// else next is the new node
 				}
-			} else {	// if before
-				dummy += 10; // char
-				( int * ) dummy += 2;
+			} else {	// if it should be before
+				dummy = ( char ** ) ( ( char * ) dummy + 10 );
+				dummy = ( char ** ) ( ( int * ) dummy + 2 ); // *prev
 				if ( *dummy != NULL ) {
 					// set prev's next as current
-					dummyHelper = *dummy;
-					dummyHelper += 11; // char
-					( int * ) dummy += 2;
+					dummyHelper = ( char ** ) *dummy;
+					dummyHelper = ( char ** ) ( ( char * ) dummyHelper + 10 );
+					dummyHelper = ( char ** ) ( ( int * ) dummyHelper + 2 );
+					dummyHelper = ( char ** ) ( ( char ** ) dummyHelper + 1 );	// *next
 					*dummyHelper = node;
 				}
 
-				dummy += 1; // char
+				dummy = ( char ** ) ( ( char * ) dummy + 1 );
 				if ( *dummy != NULL ) {
 					// set next's prev as current
-					dummyHelper = *dummy;
-					dummyHelper += 10; // char
-					( int * ) dummy += 2;
+					dummyHelper = ( char ** ) *dummy;
+					dummyHelper = ( char ** ) ( ( char * ) dummyHelper + 10 );
+					dummyHelper = ( char ** ) ( ( int * ) dummyHelper + 2 );
 					*dummyHelper = node;
 				}
 			}
 		}
 
-	} else if ( *userCount == 0 ) {	// empty case
+	} else {	// empty case
 		// set new node as head
 		*head = node;
 
 		// set prev and next to null
-		dummy = node;
-		dummy += 10; // char
-		( int * ) dummy += 2;
+		dummy = ( char ** ) node;
+		dummy = ( char ** ) ( ( char * ) dummy + 10 );
+		dummy = ( char **) ( ( int * ) dummy + 2 );	// *prev
 		*dummy = NULL;
-		dummy += 1; // char
+		dummy += 1;	// *next
 		*dummy = NULL;
 	}
 
@@ -220,28 +235,31 @@ Pop
 ====================
 */
 void Pop( int *userCount ) {
-	// |count|input|test|test2|i|name[10]|*head|
-	// 0     1     2    3     4 5					* int        
-	// 							      + 10 	  11	* char 
+	// buffer
+	// |count|input|test|test2|i|name[10]|*head|*dum1|*dum2|
+	// 0     1     2    3     4 5							 * int        
+	// 							      + 10					 * char
+	//										 + 1	 2     3 * char*
 
+	// node
 	// |name[10]|age|phone|*prev|*next|
-	// 0        10						* char
-	//			  + 1     2 			* int
-	//						  + 1     2 * char
+	// 0        10						  * char
+	//			  + 1     2 			  * int
+	//						   + 1    2 * char*
 
 	if ( *userCount == 0 ) {	// empty case
 		printf( "Nenhum contato no sistema. \n" );
 		return;
 	}
 
-	int *test = userCount + 2, *holder = test + 1, *i = test + 2, *j = userCount + 1;
+	int *test = userCount + 2, *holder = test + 1, *i = test + 2;
 	*test = 1;
 
-	char *name = ( char * ) i + sizeof( int ), *head = userCount, *node, *dummy, *dummyHelper, *dummyHelper2;
-	( int * ) name += 1;
-	( int * ) head += 5;
-	head += 10; // char
-	dummy = *head;
+	char *name = ( char * ) ( i + 1 ), **head = ( char ** ) ( userCount + 5 ), *node, **dummy, **dummyHelper, **dummyHelper2;
+	head = ( char ** ) ( ( char * ) head + 10 );
+	dummy = head + 1;
+	dummyHelper = dummy + 1;
+	dummyHelper2 = dummyHelper + 1;
 
 	printf( "Digite o nome a ser removido: " );
     scanf( "%9s", name );	// swap w fgets for spaces
@@ -251,50 +269,60 @@ void Pop( int *userCount ) {
 	*name = '\0'; // just to be safe
 	name -= 9;
 
+	dummy = ( char ** ) *head;
+
+	// removal
 	for ( *i = 0; *i < *userCount; *i += 1 ) {
-		if ( strcmp( *name, *dummy ) == 0 ) {
-			node = dummy;
+		if ( strcmp( name, *dummy ) == 0 ) {	// if current node = input
+			node = ( char * ) dummy;
 			*test = 0;
 			break;
+		}	// else try next node
+		dummy = ( char ** ) ( ( char * ) dummy + 10 );
+		dummy = ( char ** ) ( ( int * ) dummy + 2 );
+		dummy += 1;	// *next
+		if ( *dummy != NULL ) {	// just to be safe
+			dummyHelper = ( char ** ) *dummy; // maybe there's no need for the helper?
+			dummy = dummyHelper;
 		}
-		dummy += 11; // char
-		( int * ) dummy += 2;
-		dummyHelper = *dummy;
-		dummy = dummyHelper;
 	}
 
-	if ( *test == 0 ) {
-		dummy += 10; // char
-		( int * ) dummy += 2;
-		dummyHelper = dummy + 1;
-		if ( *dummy != NULL && *dummyHelper != NULL ) {	// middle case
+	if ( *test == 0 ) { // if successful in finding name
+		dummy = ( char ** ) ( ( char * ) dummy + 10 );
+		dummy = ( char ** ) ( ( int * ) dummy + 2 ); // *prev
+
+		dummyHelper = dummy + 1; // *next
+		if ( *dummy != NULL && *dummyHelper != NULL ) { // middle case
 			// set prev's next as next
-			dummyHelper2 = *dummy;
+			dummyHelper2 = ( char ** ) *dummy; // 2 to prev
 			dummy = dummyHelper2;
-			dummy += 11; // char
-			( int * ) dummy += 2;
+			dummy = ( char ** ) ( ( char * ) dummy + 10 );
+			dummy = ( char ** ) ( ( int * ) dummy + 2 );
+			dummy += 1; // *next
 			*dummy = *dummyHelper;
 			
 			// set next's prev as prev
-			dummyHelper += 10; // char
-			( int * ) dummyHelper += 2;
-			*dummyHelper = dummyHelper2;
-		} else if ( *dummy == NULL && *dummyHelper != NULL ) {	// first case
+			dummy = ( char ** ) *dummyHelper; // 0 to next
+			dummy = ( char ** ) ( ( char * ) dummy + 10 );
+			dummy = ( char ** ) ( ( int * ) dummy + 2 ); // *prev
+			*dummy = dummyHelper2;
+		} else if ( *dummy == NULL && *dummyHelper != NULL ) { // first case
 			// set next as head
 			dummy = *dummyHelper;
 			*head = dummy;
 
 			// set next's prev as NULL
-			dummy += 10; // char
-			( int * ) dummy += 2;
+			dummy = ( char ** ) ( ( char * ) dummy + 10 );
+			dummy = ( char ** ) ( ( int * ) dummy + 2 ); // *prev
 			*dummy = NULL;
-		} else if ( *dummy != NULL && *dummyHelper == NULL ) {	// last case
+		} else if ( *dummy != NULL && *dummyHelper == NULL ) { // last case
 			// set prev's next as NULL
-			dummyHelper = *dummy;
-			dummyHelper += 11; // char
-			( int * ) dummyHelper += 2;
+			dummyHelper = *dummy; // 1 to prev
+			dummyHelper = ( char ** ) ( ( char * ) dummyHelper + 10 );
+			dummyHelper = ( char ** ) ( ( int * ) dummyHelper + 2 );
+			dummyHelper += 1; // *next
 			*dummyHelper = NULL;
-		} else if ( *dummy == NULL && *dummyHelper == NULL ) {	// single case
+		} else if ( *dummy == NULL && *dummyHelper == NULL ) { // single case
 			*head = NULL;
 		}
 		free( node );
